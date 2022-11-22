dc708cf527134e7815e847c7e2b843f97f17317c
Zhuoyuan <75076820+ZLI-afk@users.noreply.github.com>
2022-11-22 18:09:19 +0800

    fix format error when add constrains to POSCAR generated

--- dpgen/auto_test/Gamma.py
+++ dpgen/auto_test/Gamma.py
@@ -298,16 +298,16 @@ class Gamma(Property):
             'true': 'F',
             'false': 'T'
         }
-        add_fix_str = ' ' + fix_dict[self.add_fix[0]] + \
+        add_fix_str = fix_dict[self.add_fix[0]] + \
                       ' ' + fix_dict[self.add_fix[1]] + \
-                      ' ' + fix_dict[self.add_fix[2]] + ' '
+                      ' ' + fix_dict[self.add_fix[2]]
         with open(poscar, 'r') as fin1:
             contents = fin1.readlines()
             contents.insert(insert_pos-1, 'Selective dynamics\n')
             for ii in range(insert_pos, 0, 1):
                 new_pos_str = contents[ii].split(' ')
                 new_pos_str.insert(-1, add_fix_str)
-                contents[ii] = ''.join(new_pos_str)
+                contents[ii] = ' '.join(new_pos_str)
         with open(poscar, 'w') as fin2:
             for ii in range(len(contents)):
                 fin2.write(contents[ii])
